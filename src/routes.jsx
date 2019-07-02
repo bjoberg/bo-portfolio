307689d6cae359fbd121e4998dbde4b659439923
Brett Oberg <brett@obergmail.com>
2019-07-01 19:23:37 -0500

    Add error page
    - Add notes for routing

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import DashboardPage from './pages/dashboard/dashboard.page';
+import ErrorPage from './pages/error/error.page';
 import ImagesListPage from './pages/images-list/images-list.page';
 
 const Routes = () => (
+  // /dashboard/images => EntityListPage
+  // /dashboard/image => EntityDetailsPage
+  // /dashboard/image/:id => EntityDetailsPage
+
+  // /dashboard/groups => EntityListPage
+  // /dashboard/group => EntityDetailsPage
+  // /dashboard/group/:id => EntityDetailsPage
+
+  // /dashboard/tags => EntityListPage
+  // /dashboard/tag => EntityDetailsPage
+  // /dashboard/tag/:id => EntityDetailsPage
+
   <Switch>
     <Route
       exact
@@ -18,6 +31,7 @@ const Routes = () => (
       path="/dashboard/image/:id"
       component={DashboardPage}
     />
+    <Route component={ErrorPage} />
   </Switch>
 );
 
