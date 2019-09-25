e49bfd53333e40236411a8a231adc6b7f17b6e8f
bjoberg <brett.oberg8@gmail.com>
2019-09-24 21:55:50 -0500

    Remove margin from button image
    - Add container to login page
    - Center the button

--- src/pages/login/login.page.jsx
+++ src/pages/login/login.page.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
 
+import LoginPageStyles from './login.styles';
 import GoogleLoginButton from '../../components/google-login-button/google-login-button.component';
 
-const LoginPage = () => <GoogleLoginButton />;
+const useStyles = makeStyles(LoginPageStyles);
+
+const LoginPage = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <GoogleLoginButton />
+    </div>
+  );
+};
 
 export default LoginPage;
