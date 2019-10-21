b2395824ed98d143c42322c75e0b24f3e23f8f37
bjoberg <brett.oberg8@gmail.com>
2019-10-20 20:12:09 -0500

    Create Profile Popover component
    - Update theme

---
+++ src/components/header/components/profile-popover/profile-popover.component.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Popover, Typography, List, ListItem, ListItemText, Divider,
+} from '@material-ui/core';
+
+import ProfilePopoverStyles from './profile-popover.styles';
+
+const useStyles = makeStyles(ProfilePopoverStyles);
+
+const ProfilePopover = (props) => {
+  const classes = useStyles();
+  const {
+    isOpen, anchorEl, handleClose, name, email, role,
+  } = props;
+
+  return (
+    <Popover
+      id="profile-popover"
+      open={isOpen}
+      anchorEl={anchorEl}
+      onClose={handleClose}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+    >
+      <div className={classes.container}>
+        <Typography variant="subtitle1">
+          {name}
+        </Typography>
+        <Typography variant="subtitle2">
+          {email}
+        </Typography>
+        <Typography variant="subtitle2">
+          {role}
+        </Typography>
+      </div>
+      <Divider />
+      <List component="nav" dense aria-label="sign out">
+        <ListItem button>
+          <ListItemText primary="Sign out" />
+        </ListItem>
+      </List>
+    </Popover>
+  );
+};
+
+ProfilePopover.propTypes = {
+  isOpen: PropTypes.bool,
+  // eslint-disable-next-line react/forbid-prop-types
+  anchorEl: PropTypes.object,
+  handleClose: PropTypes.func,
+  name: PropTypes.string,
+  email: PropTypes.string,
+  role: PropTypes.string,
+};
+
+ProfilePopover.defaultProps = {
+  isOpen: false,
+  anchorEl: {},
+  handleClose: () => { },
+  name: undefined,
+  email: undefined,
+  role: undefined,
+};
+
+export default ProfilePopover;
