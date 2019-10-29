5818f714d276f20fc894d56fc758c5fcce403e2f
bjoberg <brett.oberg8@gmail.com>
2019-10-28 21:28:11 -0500

    Use file loader instead of url loader
    - Add /dev to .gitignore

--- config/webpack.client.config.js
+++ config/webpack.client.config.js
@@ -35,7 +35,7 @@ module.exports = {
         test: /\.(png|jpg|gif|ico)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: 'file-loader',
             options: {
               cacheDirectory: true,
               name: 'static/media/[name].[contenthash:8].[ext]',
