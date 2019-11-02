18176d48fb4f0320e7238415c1dd50b66076066c
bjoberg <brett.oberg8@gmail.com>
2019-11-02 17:01:32 -0500

    Implement useInfiniteScrollHook

--- src/pages/image-list/image-list.page.jsx
+++ src/pages/image-list/image-list.page.jsx
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, {
+  Fragment, useState, useEffect, useCallback,
+} from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { LinearProgress, makeStyles, Button } from '@material-ui/core';
+import {
+  LinearProgress, makeStyles, CircularProgress,
+} from '@material-ui/core';
 
 import ImageListPageStyles from './image-list.styles';
 import ImageService from '../../services/image.service';
 import Image from '../../components/image/image.component';
+import useInfiniteScroll from '../../hooks/infinite-scroll.hook';
 
 const imageService = new ImageService();
 const useStyles = makeStyles(ImageListPageStyles);
+const evaluateIsEnd = (total, offset, nextPage) => (total / offset) <= nextPage;
 
 const ImageListPage = (props) => {
   const { openSnackbar } = props;
@@ -17,17 +23,37 @@ const ImageListPage = (props) => {
 
   const [pageIsLoaded, setPageIsLoaded] = useState(false);
   const [images, setImages] = useState([]);
-  const [page, setPage] = useState(0);
-  const [isEnd, setIsEnd] = useState(false);
+  const [imagesPage, setImagesPage] = useState(0);
+  const [isEndOfImages, setIsEndOfImages] = useState(false);
+  const [hasErrorFetchingImages, setHasErrorFetchingImages] = useState(false);
 
-  const evaluateIsEnd = (total, offset, nextPage) => (total / offset) <= nextPage;
+  const handlePaginateImages = useCallback((isFetching) => {
+    const paginateImages = async () => {
+      try {
+        const next = imagesPage + 1;
+        const result = await imageService.getImages(limit, next);
+        setImages(prevState => [...prevState, ...result.data]);
+        setIsEndOfImages(evaluateIsEnd(result.totalItems, limit, next + 1));
+        isFetching(false);
+        setImagesPage(next);
+      } catch (error) {
+        setHasErrorFetchingImages(true);
+        isFetching(false);
+        openSnackbar('error', `${error.message} Refresh to try again.`);
+      }
+    };
+    paginateImages();
+  }, [openSnackbar, imagesPage]);
+
+  const [isFetchingImages] = useInfiniteScroll(handlePaginateImages, isEndOfImages,
+    hasErrorFetchingImages);
 
   useEffect(() => {
     const getInitialImages = async () => {
       try {
         setPageIsLoaded(false);
         const result = await imageService.getImages(limit);
-        setIsEnd(evaluateIsEnd(result.totalItems, limit, 1));
+        setIsEndOfImages(evaluateIsEnd(result.totalItems, limit, 1));
         setImages(result.data);
       } catch (error) {
         // TODO: Show error message with retry option
@@ -38,44 +64,30 @@ const ImageListPage = (props) => {
     getInitialImages();
   }, []);
 
-  const handlePaginateImages = async () => {
-    try {
-      const next = page + 1;
-      const result = await imageService.getImages(limit);
-      setIsEnd(evaluateIsEnd(result.totalItems, limit, next + 1));
-      setPage(next);
-      setImages([...images, ...result.data]);
-    } catch (error) {
-      openSnackbar('error', error.message);
-    }
-  };
-
-  if (!pageIsLoaded) {
-    return (
-      <div className={classes.progressBarContainer}>
-        <LinearProgress />
-      </div>
-    );
-  }
-
   return (
-    <div className={classes.root}>
-      {images.map(item => (
-        <Link to={`/image/${item.id}`} className={classes.link}>
-          <Image
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            imageUrl={item.imageUrl}
-          />
-        </Link>
-      ))}
-      {!isEnd && (
-        <Button variant="contained" onClick={() => handlePaginateImages()}>
-          Default
-        </Button>
+    <Fragment>
+      {!pageIsLoaded && (
+        <div className={classes.linearProgressContainer}>
+          <LinearProgress />
+        </div>
       )}
-    </div>
+      <div className={classes.imageList}>
+        {images.map(item => (
+          <Link key={item.id} to={`/image/${item.id}`} className={classes.link}>
+            <Image
+              id={item.id}
+              title={item.title}
+              imageUrl={item.imageUrl}
+            />
+          </Link>
+        ))}
+      </div>
+      {isFetchingImages && (
+        <div className={classes.circularProgressContainer}>
+          <CircularProgress />
+        </div>
+      )}
+    </Fragment>
   );
 };
 
