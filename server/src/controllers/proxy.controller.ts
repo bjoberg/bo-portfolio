f437c9754452547a5fe0e42f710c1c2e6b30b6f9
bjoberg <brett.oberg8@gmail.com>
2019-11-10 20:47:15 -0600

    Update api proxy for put, post, and delete routes
    - Create proxyController

---
+++ server/src/controllers/proxy.controller.ts
@@ -0,0 +1,60 @@
+import { Express } from "express";
+import proxy from "express-http-proxy";
+import { Request } from "express-serve-static-core";
+
+import { RequestOptions } from "http";
+import IGoogleAuthorization from "../models/google-authorization.interface";
+import HttpMethods from "../models/http-methods";
+
+export default class ProxyController {
+  constructor(private app: Express, private proxyEndpoint: string, private apiEndpoint: string) { }
+
+  /**
+   * Initialize the proxy based on object properties
+   */
+  public initialize() {
+    // Redeclare variables to get the correct scope
+    const proxyEndpoint = this.proxyEndpoint;
+    const apiEndpoint = this.apiEndpoint;
+    const proxyReqPathResolver = this.proxyReqPathResolver;
+    const shouldAddAuthorization = this.shouldAddAuthorization;
+
+    // Initialize the proxy
+    this.app.use(proxyEndpoint, proxy(apiEndpoint, {
+      proxyReqPathResolver(req: Request) { return proxyReqPathResolver(req); },
+      proxyReqOptDecorator(proxyReqOpts: RequestOptions, srcReq: Request) {
+        if (shouldAddAuthorization) {
+          const user = srcReq.user as IGoogleAuthorization;
+          proxyReqOpts.headers = {
+            "Authorization": `Bearer ${user.accessToken}`,
+            "Content-Type": "application/json"
+          };
+        }
+        return proxyReqOpts;
+      },
+    }));
+  }
+
+  /**
+   * Construct and resolve the proxies path
+   */
+  private proxyReqPathResolver = (req: Request): string => {
+    return `${this.proxyEndpoint}${req.url}`;
+  }
+
+  /**
+   * Determine if the authorization headers should added or not
+   */
+  private shouldAddAuthorization = (method: string): boolean => {
+    switch (method) {
+      case HttpMethods.post:
+        return true;
+      case HttpMethods.put:
+        return true;
+      case HttpMethods.delete:
+        return true;
+      default:
+        return false;
+    }
+  }
+}
