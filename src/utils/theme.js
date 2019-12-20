7d9be667c3f35cd946bf1e976388f0839020e9c2
Brett Oberg <brett.oberg8@gmail.com>
2019-12-20 13:18:57 -0600

    Add 'Call to Action' button on Home Page & Update Material UI (#145)
    - Update material, update palette, add call to action
    - Add hover styles to full drawer selected list item

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,21 +1,15 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import { green, deepOrange, grey } from '@material-ui/core/colors';
 
 const miniDrawerWidth = 80;
 const drawerWidth = 250;
 
 const theme = createMuiTheme({
   palette: {
-    primary: {
-      light: '#e5ffff',
-      main: '#b2dfdb',
-      dark: '#82ada9',
-      contrastText: '#006064',
-    },
+    primary: green,
+    secondary: deepOrange,
     background: {
       default: '#ffffff',
-    },
-    action: {
-      hover: '#e5ffff',
     },
   },
   typography: {
@@ -26,7 +20,7 @@ const theme = createMuiTheme({
       fontWeight: 900,
     },
     subtitle2: {
-      color: '#616161', // default grey[700]
+      color: grey[700],
     },
   },
   image: {
