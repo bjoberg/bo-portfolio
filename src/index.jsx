4ad7571bc22b5eba8cae967c251ffaa444534d75
Brett Oberg <brett.oberg8@gmail.com>
2020-02-10 21:51:54 -0600

    Enable service worker (#181)

--- src/index.jsx
+++ src/index.jsx
@@ -23,7 +23,9 @@ if (process.env.NODE_ENV === 'development' && module.hot) {
   module.hot.accept();
 }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (process.env.NODE_ENV === 'production') {
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.register();
+}
