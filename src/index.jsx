bdbc809998b9149a95b8973ad83f2034265d2d2a
bjoberg <brett.oberg8@gmail.com>
2020-02-10 22:02:44 -0600

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -23,9 +23,7 @@ if (process.env.NODE_ENV === 'development' && module.hot) {
   module.hot.accept();
 }
 
-if (process.env.NODE_ENV === 'production') {
-  // If you want your app to work offline and load faster, you can change
-  // unregister() to register() below. Note this comes with some pitfalls.
-  // Learn more about service workers: https://bit.ly/CRA-PWA
-  serviceWorker.register();
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
