b793bd1cea35e2e7ff269cdc916d3c64bdf533c1
bjoberg <brett.oberg8@gmail.com>
2020-03-16 20:40:27 -0500

    Add userRole endpoint to fetch user role when logged in

---
+++ pages/api/user/[googleId]/role.js
@@ -0,0 +1,11 @@
+import fetch from 'isomorphic-unfetch';
+
+export default async (req, res) => {
+  try {
+    const result = await fetch(`${process.env.BO_API_ENDPOINT}/user/${req.query.googleId}/role`);
+    const json = await result.json();
+    res.status(200).json(json);
+  } catch (error) {
+    res.status(error.status || 500).end(error.message);
+  }
+}
