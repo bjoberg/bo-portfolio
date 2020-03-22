8cb65111afcffa98a0fabe94dd96482f514abcd7
bjoberg <brett.oberg8@gmail.com>
2020-03-22 18:57:24 -0500

    Start profile page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles, Grid, Avatar } from '@material-ui/core';
 
+import IndexStyles from '../styles';
 import AppContainer from '../components/AppContainer';
 import User from '../models/user';
 
+const useStyles = makeStyles(IndexStyles);
+
 const Index = (props) => {
+  const classes = useStyles();
   const { user } = props;
 
   const actionBarOptions = {
@@ -16,7 +21,15 @@ const Index = (props) => {
 
   return (
     <AppContainer user={user} actionBarOptions={actionBarOptions}>
-      <div>Index</div>
+      <div className={classes.background}>
+        <div style={{ height: 300 }} />
+        <Grid style={{ backgroundColor: 'white', minHeight: '100vh' }} container>
+          <Grid item>
+            <Avatar style={{ margin: '-20' }} />
+          </Grid>
+        </Grid>
+
+      </div>
     </AppContainer>
   );
 };
---
+++ styles/IndexStyles.js
@@ -0,0 +1,17 @@
+const IndexStyles = {
+  background: {
+    /* The image used */
+    backgroundImage: "url('../static/media/20190912-Acadia-263.jpg')",
+
+    /* Set a specific height */
+    height: '100vh',
+
+    /* Create the parallax scrolling effect */
+    backgroundAttachment: 'fixed',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+};
+
+export default IndexStyles;
