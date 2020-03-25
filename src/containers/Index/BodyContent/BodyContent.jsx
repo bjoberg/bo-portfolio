64e93e5bd1a4c7787a89f13baed5d8c793ee2753
bjoberg <brett.oberg8@gmail.com>
2020-03-25 09:11:08 -0500

    Update BodyContent.jsx

--- src/containers/Index/BodyContent/BodyContent.jsx
+++ src/containers/Index/BodyContent/BodyContent.jsx
@@ -5,7 +5,7 @@ import { Grid, Typography } from '@material-ui/core';
 const BodyContent = () => (
   <Grid container spacing={4}>
     <Grid item>
-      <Typography variant="body1" gutterBottom>Heyyooo 👋🏼, thanks for taking the time to visit my photography portfolio. My fullName is Brett Oberg, and in my spare time I take photos. I am inspired by nature and I use photography as an excuse to experience its beauty. Photography has helped give direction and purpose to my life, and I hope my images inspire you to go out and find what is special for you.</Typography>
+      <Typography variant="body1" gutterBottom>Heyyooo 👋🏼, thanks for taking the time to visit my photography portfolio. My name is Brett Oberg, and in my spare time I take photos. I am inspired by nature and I use photography as an excuse to experience its beauty. Photography has helped give direction and purpose to my life, and I hope my images inspire you to go out and find what is special for you.</Typography>
     </Grid>
     <Grid item>
       <Typography variant="body1" gutterBottom>Aside from photography, I spend my time developing software (like this website 😜), cycling, golfing, playing tennis, and spening time with family.</Typography>
