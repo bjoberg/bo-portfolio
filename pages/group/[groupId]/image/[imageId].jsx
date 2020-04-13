1cdd47860be2026566afa4daf3be6a109ab283e7
bjoberg <brett.oberg8@gmail.com>
2020-04-13 17:00:51 -0500

    Add root styles and make request for group image

--- pages/group/[groupId]/image/[imageId].jsx
+++ pages/group/[groupId]/image/[imageId].jsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-const GroupImage = () => (<div>hello?</div>);
+import { GroupImageStyles } from '../../../../src/styles';
+import { getGroupImage } from '../../../../src/services/group';
+
+const useStyles = makeStyles(GroupImageStyles);
+
+const GroupImage = (props) => {
+  const classes = useStyles();
+  const { hasError, image } = props;
+  console.log(hasError);
+  console.log(image);
+  // TODO: What to display when the image does not exist within the group?
+  // TODO: Need api endpoint for group/groupId/image/{imageId}
+  // TODO: Modify GET requests to have previous entity and next entity
+  return (
+    <div className={classes.root}>
+      Test
+    </div>
+  );
+};
+
+GroupImage.getInitialProps = async ({ query }) => {
+  const { groupId, imageId } = query;
+  let hasError = false;
+  let image;
+  try {
+    image = await getGroupImage(groupId, imageId);
+  } catch (error) {
+    hasError = true;
+  }
+  return { hasError, image };
+};
 
 export default GroupImage;
---
+++ src/styles/GroupImageStyles.js
@@ -0,0 +1,9 @@
+const GroupImageStyles = theme => ({
+  root: {
+    backgroundColor: theme.palette.common.black,
+    height: '100vh',
+    width: '100vw',
+  },
+});
+
+export default GroupImageStyles;
