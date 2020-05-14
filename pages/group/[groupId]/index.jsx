b561c5a05f031d28417a5dde0a8cf92051f21f8c
bjoberg <brett.oberg8@gmail.com>
2020-05-13 22:27:03 -0500

    Cleanup 'group' page

--- pages/group/[groupId]/index.jsx
+++ pages/group/[groupId]/index.jsx
@@ -2,42 +2,48 @@ import React, {
   createRef, Fragment, useState, useCallback,
 } from 'react';
 import PropTypes from 'prop-types';
+import getConfig from 'next/config';
 import fetch from 'isomorphic-unfetch';
 import httpStatus from 'http-status';
-import { NextSeo } from 'next-seo';
-import { Typography } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { NextSeo } from 'next-seo';
 
+import SEO from '../../../next-seo.config';
 import AppContainer from '../../../src/components/AppContainer';
+import SeoConfig from '../../../src/models/SeoConfig';
 import { ImageGrid } from '../../../src/components/ImageGrid';
 import { GroupStyles } from '../../../src/styles';
 import { getGroup, getGroupImages } from '../../../src/services/group';
-import { getSEOConfigForGroup } from '../../../src/utils/seo';
 import { isAtEnd, goBack } from '../../../src/utils/helpers';
 import { useInfiniteScroll } from '../../../src/hooks';
 
+const { publicRuntimeConfig } = getConfig();
 const useStyles = makeStyles(GroupStyles);
 
 const Group = (props) => {
   const classes = useStyles();
   const imageGridRef = createRef();
+  const { group, hasError, images } = props;
   const {
-    group,
-    hasError,
-    images,
-  } = props;
+    id, title, description, thumbnailUrl,
+  } = group;
   const {
-    seoUrl,
-    seoTitle,
-    seoDescription,
-    seoImages,
-  } = getSEOConfigForGroup(group);
-  const hasMoreData = isAtEnd(images.totalItems, images.limit, images.page + 1);
+    totalItems,
+    limit,
+    page,
+    rows,
+  } = images;
+  const seoTitle = `${title} - ${SEO.title}`;
+  const url = `${publicRuntimeConfig.ROOT_URL}/group/${id}`;
+  const seoConfig = new SeoConfig(hasError, hasError, seoTitle, description, url);
+  seoConfig.pushOpenGraphImage(thumbnailUrl, undefined, undefined, `Brett Oberg ${title}`);
+  const hasMoreData = isAtEnd(totalItems, limit, page + 1);
 
   const [pageHasError, setPageHasError] = useState(hasError);
-  const [isEnd, setIsEnd] = useState(hasMoreData);
-  const [page, setPage] = useState(images.page);
-  const [items, setItems] = useState(images.rows);
+  const [isAtEndOfImageList, setIsAtEndOfImageList] = useState(hasMoreData);
+  const [currImagePage, setCurrImagePage] = useState(images.page);
+  const [imageItems, setImageItems] = useState(rows);
 
   const actionBarOptions = {
     elevateOnScroll: true,
@@ -52,66 +58,52 @@ const Group = (props) => {
    */
   const handlePaginateImages = useCallback((isFetching) => {
     const paginateImages = async () => {
-      const next = page + 1;
-      const result = await fetch(`/api/group/${group.id}/images?limit=${images.limit}&page=${next}`);
+      const next = currImagePage + 1;
+      const result = await fetch(`/api/group/${id}/images?limit=${limit}&page=${next}`);
       if (result.status === httpStatus.OK) {
         const json = await result.json();
-        setItems(prevState => [...prevState, ...json.rows]);
-        setIsEnd(isAtEnd(json.totalItems, json.limit, next + 1));
+        setImageItems(prevState => [...prevState, ...json.rows]);
+        setIsAtEndOfImageList(isAtEnd(json.totalItems, json.limit, next + 1));
         isFetching(false);
-        setPage(next);
+        setCurrImagePage(next);
       } else {
         setPageHasError(true);
         isFetching(false);
       }
     };
     paginateImages();
-  }, [group.id, images.limit, page]);
+  }, [currImagePage, id, limit]);
 
   const [isLoadingImages] = useInfiniteScroll(
     handlePaginateImages,
-    isEnd,
+    isAtEndOfImageList,
     pageHasError,
     imageGridRef,
   );
 
   return (
     <Fragment>
-      <NextSeo
-        noindex={pageHasError}
-        nofollow={pageHasError}
-        title={seoTitle}
-        description={seoDescription}
-        canonical={seoUrl}
-        openGraph={{
-          url: seoUrl,
-          title: seoTitle,
-          description: seoDescription,
-          images: seoImages,
-        }}
-      />
+      <NextSeo {...seoConfig.getConfig()} />
       <AppContainer actionBarOptions={actionBarOptions}>
-        <div className={classes.root}>
-          <div className={classes.container}>
-            <Typography variant="h1">{group.title}</Typography>
+        <Grid container className={classes.root} spacing={4}>
+          <Grid item xs={12}>
+            <Typography variant="h1">{title}</Typography>
             <Typography variant="subtitle2">
-              {images.totalItems}
-              {' '}
-              images
+              {`${totalItems} images`}
             </Typography>
             <div className={classes.subheader}>
-              <Typography>{group.description}</Typography>
+              <Typography>{description}</Typography>
             </div>
-            <div className={classes.imageGridContainer}>
-              <ImageGrid
-                domRef={imageGridRef}
-                images={items}
-                routeBase={`/group/${group.id}/image/`}
-                isLoading={isLoadingImages}
-              />
-            </div>
-          </div>
-        </div>
+          </Grid>
+          <Grid item xs={12}>
+            <ImageGrid
+              domRef={imageGridRef}
+              images={imageItems}
+              routeBase={`/group/${id}/image/`}
+              isLoading={isLoadingImages}
+            />
+          </Grid>
+        </Grid>
       </AppContainer>
     </Fragment>
   );
